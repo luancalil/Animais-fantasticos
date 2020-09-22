ffab82b71b057a8b1de435e1773009134dbf5488
Luan Calil <luancalil3@gmail.com>
2020-09-22 13:12:59 -0300

    Add smooth scroll

--- script/script.js
+++ script/script.js
@@ -41,3 +41,22 @@ function inititAccordion() {
 }
 
 inititAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
